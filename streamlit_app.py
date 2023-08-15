1d0ae320cb15741327777ca7460fece627568d9e
Amber Afshan <33459149+AfshanRasheed@users.noreply.github.com>
2023-08-15 00:47:03 -0700

    Update streamlit_app.py
    add some banana to the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit;
 streamlit.title('My better half new Healthy Dinner');
 
 streamlit.header('🥣 Breakfast Menu')
-streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg, 🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
