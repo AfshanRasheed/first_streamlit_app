052ac093f9c93f478ecf0d2c4b2dc5e7910118a1
Amber Afshan <33459149+AfshanRasheed@users.noreply.github.com>
2023-08-15 02:46:00 -0700

    Update streamlit_app.py
    Add selected item in variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,11 @@ streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg, 🍌🥭 some bananas, mang
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
----streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index) , ['Apple','Banana'] )
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index) , ['Apple','Banana'] )
+
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
