4dca2c1ff1016c047718827564294fa46be33419
Amber Afshan <33459149+AfshanRasheed@users.noreply.github.com>
2023-08-15 00:50:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title('My better half new Healthy Dinner');
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg, 🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg, 🍌🥭 some bananas, mango, graps and cherries 🥝🍇')
